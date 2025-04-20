65afb125f5b61c48a99ccf889a6e38b64dc9885f
TFA05 <b2sinadinovic@gmail.com>
2025-04-20 18:48:58 +0200

    Add functinallity to Click and Border buttons

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
-function changeColorWhenHovering(gridSquare){
-    gridSquare.addEventListener("mouseover", () => {
-        gridSquare.style.backgroundColor = "rgb(39, 28, 2)";
-    })
+document.addEventListener("mousedown", () => {
+    isMouseDown = true;
+})
+
+document.addEventListener("mouseup", () => {
+    isMouseDown = false;
+})
+
+function colorSquare(event){
+    event.target.style.backgroundColor = "rgb(39, 28, 2)";
+}
+
+function dragColor(event){
+    if (isMouseDown) event.target.style.backgroundColor = "rgb(39, 28, 2)";
 }
 
 function makeGrid(numSquares){
@@ -14,21 +24,30 @@ function makeGrid(numSquares){
         }
         const gridSquare = document.createElement("div");
         gridSquare.classList.add("gridSquare");
-
-        changeColorWhenHovering(gridSquare);
+        
+        if (isClickToDrawOn){
+            gridSquare.addEventListener("mouseover", dragColor);
+            gridSquare.addEventListener("mousedown", colorSquare);
+        }
+        else{
+            gridSquare.addEventListener("mouseover", colorSquare);
+        }
 
         gridRow.appendChild(gridSquare);
     }
+    if (isBordersOn) {
+        isBordersOn = false;
+        toggleBorders();
+    }
+        
 }
 
-let gridContainer = document.querySelector(".gridContainer");
-let ratioBtn = document.querySelector(".gridRatioBtn");
-let gameContainer = document.querySelector(".gameContainer");
+function changeNumSquares(){
+    let numSquares = prompt("How many squares should be per side? (1-100)");
 
-makeGrid(20);
-
-ratioBtn.addEventListener("click", () => {
-    let numSquares = prompt("How many squares should be per side?");
+    while (numSquares > 100){
+        numSquares = prompt("The number has to be between 1 and 100")
+    }
 
     gridContainer.remove();
 
@@ -37,5 +56,61 @@ ratioBtn.addEventListener("click", () => {
     gameContainer.insertBefore(gridContainer, document.querySelector(".btnsContainer"));
 
     makeGrid(numSquares);
+}
 
-})
+function toggleClickToDraw(){
+    let gridSquares = document.querySelectorAll(".gridSquare");
+    if (isClickToDrawOn){
+        gridSquares.forEach((gridSquare) => {
+            gridSquare.removeEventListener("mouseover", dragColor);
+            gridSquare.addEventListener("mousedown", colorSquare);
+            
+            gridSquare.addEventListener("mouseover", colorSquare);
+        })
+        isClickToDrawOn = false;    
+    }
+    else{
+        gridSquares.forEach((gridSquare) => {
+            gridSquare.removeEventListener("mouseover", colorSquare);
+            
+            gridSquare.addEventListener("mouseover", dragColor);
+            gridSquare.addEventListener("mousedown", colorSquare);
+        })
+        isClickToDrawOn = true;
+    }
+    
+}
+
+function toggleBorders(){
+    let gridSquares = document.querySelectorAll(".gridSquare")
+    if (isBordersOn){
+        gridSquares.forEach((gridSquare) => {
+            gridSquare.style.border = "0px";
+        })
+        isBordersOn = false;
+    }
+    else{
+        gridSquares.forEach((gridSquare) => {
+            gridSquare.style.border = "2px rgb(51, 30, 4) solid";
+        })
+        isBordersOn = true;
+    }
+}
+
+let gridContainer = document.querySelector(".gridContainer");
+let ratioBtn = document.querySelector(".gridRatioBtn");
+let gameContainer = document.querySelector(".gameContainer");
+let clickBtn = document.querySelector(".clickToDrawBtn");
+let borderBtn = document.querySelector(".borderBtn");
+
+let isClickToDrawOn = false;
+let isBordersOn = false;
+let isMouseDown;
+
+makeGrid(20);
+
+ratioBtn.addEventListener("click", changeNumSquares);
+
+clickBtn.addEventListener("click", toggleClickToDraw);
+
+borderBtn.addEventListener("click", toggleBorders);
